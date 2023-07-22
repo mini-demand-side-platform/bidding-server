4ae3a9735f8300763a5c51fddf3b9f947cdb019c
ray <wuhaohsiang1992@gmail.com>
2023-07-22 18:16:12 +0800

    update testcase

--- tests/test_ctr_prediction.py
+++ tests/test_ctr_prediction.py
@@ -1,7 +1,12 @@
 from unittest import TestCase
 
 from bidding_server.ctr_prediction import PredictionServerWithFeatureStore
-from bidding_server.data_template import BidRequest, EligibleAd, OnlineFeatureInput
+from bidding_server.data_template import (
+    BidRequest,
+    EligibleAd,
+    FeaturesInfo,
+    OnlineFeatureInput,
+)
 
 
 class TestCtrPrediction(TestCase):
@@ -27,6 +32,68 @@ class TestCtrPrediction(TestCase):
             width=617.5163,
             hist_ctr=0.00036614624,
             hist_cvr=6.790803e-05,
+        )
+        self.cp._features_info = FeaturesInfo(
+            feature_id=[
+                "5dbfb549",
+                "ea2f676f",
+                "0ad63312",
+                "a2480212",
+                "5aae0b22",
+                "65934929",
+                "2658049e",
+                "717456fc",
+                "1d34889f",
+                "915a0c3c",
+            ],
+            feature_name=[
+                "layout_style_AB",
+                "layout_style_RU",
+                "layout_style_GY",
+                "layout_style_MR",
+                "layout_style_BK",
+                "layout_style_BX",
+                "layout_style_RZ",
+                "layout_style_TY",
+                "category_Shirt",
+                "layout_style_DX",
+            ],
+            source_table_name=[
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+                "ctr",
+            ],
+            source_column_name=[
+                "layout_style",
+                "layout_style",
+                "layout_style",
+                "layout_style",
+                "layout_style",
+                "layout_style",
+                "layout_style",
+                "layout_style",
+                "category",
+                "layout_style",
+            ],
+            feature_function_type=[
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+                "string_mapping",
+            ],
         )
         res = self.cp._get_online_feature_input(
             eligible_ad=test_ad, bid_request=test_bid_request
